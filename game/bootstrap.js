a587833e0df4fa167eff5484bb34d797efef2e53
Fox Wilson <fwilson@fwilson.me>
2014-12-05 21:23:27 -0500

    change color of background, add index

--- game/bootstrap.js
+++ game/bootstrap.js
@@ -2,5 +2,5 @@ var GAME_HEIGHT = 768;
 var GAME_WIDTH = 768;
 window.onload = function() {
     Crafty.init(GAME_WIDTH, GAME_HEIGHT);
-    Crafty.background('#eee');
+    Crafty.background('#ccc');
 }
